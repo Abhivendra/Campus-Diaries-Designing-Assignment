5964a9f35f463a872bce2f22cf24c0b5a9d7fbca
Abhivendra Singh <abhivendrasingh@gmail.com>
2015-12-16 08:54:46 +0530

    Css added

--- js/script.js
+++ js/script.js
@@ -8,40 +8,23 @@
 			    $this.data('scrollTimeout', setTimeout(callback,timeout));
 			  });
 			};
-			//calling scroll end event i.e. reapperence of nav bar
-			$(window).scrollEnd(function(){
-			  $('#header-bar').fadeIn();
-			}, 400);// I am using .4 seconds for reapperence It can be 
-
+			
 			//function for tracking scroll up and scroll down events
-			var lastScrollTop = 0;
+			var lastScrollTop = 10;
 			$(window).scroll(function(event){
-			   var stopingPoint = $(this).scrollTop();
-			   if (stopingPoint > lastScrollTop){
+			  var stopingPoint = $(this).scrollTop();
+			  if (stopingPoint > lastScrollTop){
 			       // downscroll code
 			       // do nothing. Because I know nothing:P I am John Snow.
-			   }
-			   else {
-			      // upscroll code
-			      $('#header-bar').fadeOut();
-			   }
-			   lastScrollTop = stopingPoint;
-			});
+			  }
+			  else {
+			    // upscroll code
+			    $('#header-bar').fadeOut();
+			  }
+			  lastScrollTop = stopingPoint;
 
-			// Random image generator
-			//comment out following function to randomise image only once
-			// var randomImageFun=function(){
-				var imgStartPath="http://dkzstslcvgwbc.cloudfront.net/sites/all/themes/campus_diaries/images/front/";
-				var randomNumber=Math.floor((Math.random() * 10) + 1);
-				var imageRandomPath=imgStartPath+randomNumber+'.jpg';
-				console.log('imageRandomPath:' , imageRandomPath);
-				$("#random-image").attr("src",imageRandomPath);
-			// };
-			// setInterval(randomImageFun,2000);
-
-			//fixed bottom code start
-			$(window).scroll(function(event){
-				var scrollTop = $(window).scrollTop();
+			  //fixed bottom code start
+			  var scrollTop = $(window).scrollTop();
     		var footerHeight=$('#footer').height();
     		// var footerPosition =Math.round($('#footer').offset().top);
     		// console.log('$(document).height() ' , $(document).height()-footerHeight);
@@ -54,3 +37,20 @@
     			$('#bottom-fixed-strip').stop().css({'position':'fixed'});
     		}
 			});
+			//calling scroll end event i.e. reapperence of nav bar
+	    $(window).scrollEnd(function(){
+				$('#header-bar').fadeIn();
+			}, 200);// I am using .2 seconds for reapperence It can be
+			 
+			// Random image generator
+			//comment out following function to randomise image
+			// var randomImageFun=function(){
+				var imgStartPath="http://dkzstslcvgwbc.cloudfront.net/sites/all/themes/campus_diaries/images/front/";
+				var randomNumber=Math.floor((Math.random() * 10) + 1);
+				var imageRandomPath=imgStartPath+randomNumber+'.jpg';
+				// console.log('imageRandomPath:' , imageRandomPath);
+				$("#random-image").attr("src",imageRandomPath);
+			// };
+			// setInterval(randomImageFun,2000);
+
+			
